f388efc47ab926ff356bd11de264e38a8730e27c
arq-angel <87254753+arq-angel@users.noreply.github.com>
2025-07-17 23:39:55 +1000

    update the few remaining imports

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,6 +1,6 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import Navbar from "react-bootstrap/Navbar.jsx";
 import { Link } from "react-router-dom";
 
 import { IoIosCreate } from "react-icons/io";
